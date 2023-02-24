509ac4b45e81b497f9ec885023366a7520411a11
Jolene Ciccarone <jciccaro@stevens.edu>
2023-02-24 16:31:19 -0500

    Adding HW 04a

---
+++ HW 04a/mybrand.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+def my_brand(name):
+    print("=*=*=*= Jolene Ciccarone =*=*=*=")
+    print("=*=*=*= Course 2023S-SSW567-WS =*=*=*=")
+    print("=*=*=*= " + name + " =*=*=*=")
+
+    now = datetime.now()
+    # dd/mm/YY H:M:S
+    dt= now.strftime("%d/%m/%Y %H:%M:%S")
+    print("=*=*=*= " + dt + " =*=*=*=")
+
+my_brand("hello")
